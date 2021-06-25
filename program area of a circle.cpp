9cf10afe65dbfd77b732c1cc54844b4bc69569b0
mercynelima <85347024+mercynelima@users.noreply.github.com>
2021-06-25 14:25:17 +0300

    Add files via upload

---
+++ program area of a circle.cpp
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+int main(){
+	
+	float area,radius;
+	printf("enter the radius of a circle");
+	scanf("%f",&radius);
+	area=3.142*radius*radius;
+	printf("area of the circle%f",area);
+	
+	return 0;
+}
