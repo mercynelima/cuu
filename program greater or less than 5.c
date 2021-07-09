fc500fd6f1a400fb61a15f428af92f5dcfbb5ef3
mercynelima <85347024+mercynelima@users.noreply.github.com>
2021-07-08 23:58:10 -0700

    Add files via upload

---
+++ program greater or less than 5.c
@@ -0,0 +1,15 @@
+//program to check number is greater than dvmbps
+#include<stdio.h>
+
+int main(){
+	int a;
+		printf("enter the value of a");
+		scanf("%d",&a);	
+		if(a>5){
+			printf("that number is greater than 5");
+	}
+	else{
+		printf("that number is less than 5");
+	}
+}	
+
